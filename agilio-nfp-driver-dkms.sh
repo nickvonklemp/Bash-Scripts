5ad7a7af816dacdaddf7495db6ba85968e0f0f18
netronome-support <netronome-support@netronome.com>
2018-05-16 16:48:56 +0200

    Commit 006

--- agilio-nfp-driver-dkms.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-wget http://172.26.1.131/nas/CoreNIC/agilio-nfp-driver-dkms_2018.01.11.2333.f40482a_all.deb
-dpkg -i agilio-nfp-driver-dkms_2018.01.11.2333.f40482a_all.deb
-apt-get -y install -f
-echo "Cleaning"
-rm -r agilio-nfp-driver-dkms_2018.01.11.2333.f40482a_all.deb
-
