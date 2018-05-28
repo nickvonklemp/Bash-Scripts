21f55681366aea72f1ad5675a614425ca39d7d70
root <root@localhost.localdomain>
2018-05-28 08:16:33 +0200

    Commit 016 Added switch for centos/rhel when adding netronome repos to package manager

--- install/netronome-repo.sh
+++ install/netronome-repo.sh
@@ -1,15 +1,37 @@
 #Adds netronome's public repo to the APT sources list
 #!/bin/sh
 script_dir=$(pwd)
-echo "Downloading netronome public key"
-wget https://deb.netronome.com/gpg/NetronomePublic.key
-echo "Adding public key to apt package"
-apt-key add NetronomePublic.key
-echo "Updated source list"
-mkdir -p /etc/apt/sources.list.d/
-echo "deb https://deb.netronome.com/apt stable main" > /etc/apt/sources.list.d/netronome.list
+if [ "$(cat /etc/os-release | grep "ID=ubuntu" | cut -d '=' -f2)" == "ubuntu" ]; then
+	# First download debian package key
+	echo "Downloading netronome public key"
+	wget https://deb.netronome.com/gpg/NetronomePublic.key
+    # Ubuntu specific apt-get changes
+	echo "Adding public key to apt package"
+	apt-key add NetronomePublic.key
+	# Add netronome's repository
+	mkdir -p /etc/apt/sources.list.d/
+	echo "deb https://deb.netronome.com/apt stable main" > /etc/apt/sources.list.d/netronome.list
+	echo "Done, updating packages"
+	# Update repository lists
+	apt-get update
+fi
+if [ "$(cat /etc/os-release | grep 'ID="centos"' | cut -d '"' -f2 | cut -d '"' -f1)" == "rhel" ] | [ "$(cat /etc/os-release | grep 'ID="rhel"' | cut -d '"' -f2 | cut -d '"' -f1)" == "centos" ]; then
+	# First download rpm package key
+	echo "Downloading netronome public key"
+	wget https://rpm.netronome.com/gpg/NetronomePublic.key
+	# RHEL/Centos specific yum changes
+	echo "Adding public key to yum package"
+	rpm --import NetronomePublic.key
+	# Add netronome's repository
+	cat << EOF >> /etc/yum.repos.d/netronome.repo
+[netronome]
+name=netronome
+baseurl=https://rpm.netronome.com/repos/centos
+EOF
+	# Update repository lists
+	echo "Done, updating packages"
+	yum update
+fi
 echo "cleaning"
 rm -r $script_dir/NetronomePublic.key
-echo "Done, updating packages"
-apt-get update
 echo "Complete"
