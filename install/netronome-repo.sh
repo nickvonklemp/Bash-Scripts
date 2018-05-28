3a96eb8c695cd7f42c7cd386ab6b7ab2d1cdb12d
nickvonklemp <nick.vonklemp@gmail.com>
2018-05-28 07:32:31 +0000

    Commit 017 Corrected OS detection if elses

--- install/netronome-repo.sh
+++ install/netronome-repo.sh
@@ -1,21 +1,29 @@
-#Adds netronome's public repo to the APT sources list
-#!/bin/sh
+#!/bin/bash
+# Adds netronome repo list to package manger
 script_dir=$(pwd)
-if [ "$(cat /etc/os-release | grep "ID=ubuntu" | cut -d '=' -f2)" == "ubuntu" ]; then
+echo $script_dir
+RHEL=$(cat /etc/os-release | grep 'ID="rhel"' | cut -d '"' -f2 | cut -d '"' -f1)
+CENTOS=$(cat /etc/os-release | grep 'ID="centos"' | cut -d '"' -f2 | cut -d '"' -f1)
+UBUNTU=$(cat /etc/os-release | grep "ID=ubuntu" | cut -d '=' -f2)
+if [ "$UBUNTU" == "ubuntu" ]; then
+	echo "Ubuntu system detected"
 	# First download debian package key
 	echo "Downloading netronome public key"
 	wget https://deb.netronome.com/gpg/NetronomePublic.key
-    # Ubuntu specific apt-get changes
+        # Ubuntu specific apt-get changes
 	echo "Adding public key to apt package"
 	apt-key add NetronomePublic.key
 	# Add netronome's repository
-	mkdir -p /etc/apt/sources.list.d/
+	mkdir -p /etc/apt/sources.list.d
 	echo "deb https://deb.netronome.com/apt stable main" > /etc/apt/sources.list.d/netronome.list
 	echo "Done, updating packages"
 	# Update repository lists
 	apt-get update
+	echo "Cleaning up"
+	rm -r NetronomePublic.key
 fi
-if [ "$(cat /etc/os-release | grep 'ID="centos"' | cut -d '"' -f2 | cut -d '"' -f1)" == "rhel" ] | [ "$(cat /etc/os-release | grep 'ID="rhel"' | cut -d '"' -f2 | cut -d '"' -f1)" == "centos" ]; then
+if [[ ( "$RHEL" == "rhel" ) || ( "$CENTOS" == "centos" ) ]]; then
+	echo "RHEL/Centos system detected"
 	# First download rpm package key
 	echo "Downloading netronome public key"
 	wget https://rpm.netronome.com/gpg/NetronomePublic.key
@@ -31,7 +39,8 @@ EOF
 	# Update repository lists
 	echo "Done, updating packages"
 	yum update
+	echo "Cleaning up"
+	rm -r -f NetronomePublic.key
 fi
-echo "cleaning"
-rm -r $script_dir/NetronomePublic.key
-echo "Complete"
+
+
